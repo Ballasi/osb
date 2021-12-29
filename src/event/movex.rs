794d8d075266edbe548b1ada3e8a4db6bafa9272
DoomDuck <docdoomduck@protonmail.com>
2021-12-29 19:34:58 +0100

    Remove license header

--- src/event/movex.rs
+++ src/event/movex.rs
@@ -1,11 +1,3 @@
-// Copyright 2021 Thomas Ballasi
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 use crate::easing::Easing;
 use crate::utils::Number;
 use crate::Event;
